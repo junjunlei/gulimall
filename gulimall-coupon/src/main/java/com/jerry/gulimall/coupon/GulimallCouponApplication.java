6935bbf3f6cf8452b5732caab5003259807ff96f
雷俊俊24808 <junjun.lei@hand-china.com>
2021-10-10 16:45:16 +0800

    [ADD]:初始化各个项目

--- gulimall-coupon/src/main/java/com/jerry/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/jerry/gulimall/coupon/GulimallCouponApplication.java
@@ -2,6 +2,7 @@ package com.jerry.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author jerry
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2021-10-10 13:09
  * @Version 1.0
  **/
+@EnableEurekaClient
 @SpringBootApplication
 public class GulimallCouponApplication
 {
